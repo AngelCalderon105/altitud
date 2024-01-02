822c69289d2582a0562ece83b5fb6f5bd92a0b88
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2024-01-01 21:30:23 -0800

    added footer component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './HomePage/Home'
+import Footer from './FooterPage/Footer'
 
 function App() {
 
   return (
     <div className='font-times text-white mx-5'>
       <Home/>
+      <Footer/>
     </div>
       
   )
